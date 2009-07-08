0e0c212dc12ebbb80a1058b69de3ecd8b862959e
kolanos <kolanos@gmail.com>
2009-07-07 21:19:04 -0700

    Adding README, some minor changes

--- models/tagging.php
+++ models/tagging.php
@@ -11,7 +11,7 @@ class Tagging_Model extends ORM {
 		// load database library into $this->db (can be omitted if not required)
 		parent::__construct($id);
 	
-		// Set the DATETIME for use with created and modified fields
+		// Set the UNIX timestamp for use with the created field
 		$this->now = time();
 	}
 
@@ -28,20 +28,5 @@ class Tagging_Model extends ORM {
 
 		return parent::save();
 	}
-	
-	// Find similar tags
-	public function similar($tags_table, $tagged_table, $tag, $user_id, $limit)
-	{
-		if (isset($user_id) and intval($user_id) > 0) 
-		{
-			$user_id = intval($user_id);
-			$where_sql .= " AND o1.tagger_id = $user_id AND o2.tagger_id = $user_id ";
-		}
-	
-		$result = $this->db->query("SELECT t1.tag, COUNT( o1.object_id ) AS quantity FROM ? o1 INNER JOIN ? t1 ON ( t1.id = o1.tag_id ) INNER JOIN ? o2 ON ( o1.object_id = o2.object_id ) INNER JOIN ? t2 ON ( t2.id = o2.tag_id ) WHERE t2.tag = ? AND t1.tag != ? ? GROUP BY o1.tag_id ORDER BY quantity DESC LIMIT 0, ?", $tagged_table, $tags_table, $tagged_table, $tags_table, $tag, $tag, $where_sql, $limit);
-	
 
-		return $result->result();
-	}
-
-} // End Tagged Model
+} // End Tagging Model
